eaa8adad861ddbf91dad9382df37133e3d7f3cca
bduvidzon <79331969+bduvidzon@users.noreply.github.com>
2021-10-30 23:37:09 -0500

    Update Calendar.aspx.cs

--- Calendar.aspx.cs
+++ Calendar.aspx.cs
@@ -16,8 +16,20 @@ namespace Senior_Project
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Request.QueryString["Email"] != null)
+            { Label4.Text = Request.QueryString["Email"]; }
             con.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True";
-
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "SELECT * FROM [User] WHERE Email='" + Label4.Text.Trim() + "'";
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                Label1.Text = dr["FirstName"].ToString().Trim();
+                Label2.Text = dr["LastName"].ToString().Trim();
+                Label3.Text = dr["ReservationID"].ToString().Trim();
+            }
+            con.Close();
         }
 
         protected void Calendar1_SelectionChanged(object sender, EventArgs e)
@@ -39,7 +51,24 @@ namespace Senior_Project
                 }
             }
             con.Close();
+        }
 
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/ResearchProjects.aspx", false);
+            Response.Redirect("~/ResearchProjects.aspx?Email=" + Label4.Text);
+        }
+
+        protected void LinkButton2_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/ResearcherReservationSchedule.aspx", false);
+            Response.Redirect("~/ResearcherReservationSchedule.aspx?Email=" + Label4.Text);;
+        }
+
+        protected void LinkButton3_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/ResearchReservation.aspx", false);
+            Response.Redirect("~/ResearchReservation.aspx?Email=" + Label4.Text);
         }
     }
 }
