3fe84069e7cdf0591d23570307e51559226ea89b
bduvidzon <79331969+bduvidzon@users.noreply.github.com>
2021-10-30 23:37:13 -0500

    Create ResearcherReservationSchedule.aspx.cs

---
+++ ResearcherReservationSchedule.aspx.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Senior_Project
+{
+    public partial class ResearcherReservationSchedule : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        SqlDataReader dr;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.QueryString["Email"] != null)
+            { Label4.Text = Request.QueryString["Email"]; }
+            con.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True";
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "SELECT * FROM [User] WHERE Email='" + Label4.Text.Trim() + "'";
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                Label1.Text = dr["FirstName"].ToString().Trim();
+                Label2.Text = dr["LastName"].ToString().Trim();
+                Label3.Text = dr["ReservationID"].ToString().Trim();
+            }
+            con.Close();
+        }
+
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Calendar.aspx", false);
+            Response.Redirect("~/Calendar.aspx?Email=" + Label4.Text);
+        }
+
+        protected void LinkButton2_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/ResearchProjects.aspx", false);
+            Response.Redirect("~/ResearchProjects.aspx?Email=" + Label4.Text);
+        }
+
+        protected void LinkButton3_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/ResearchReservation.aspx", false);
+            Response.Redirect("~/ResearchReservation.aspx?Email=" + Label4.Text);
+        }
+    }
+}
