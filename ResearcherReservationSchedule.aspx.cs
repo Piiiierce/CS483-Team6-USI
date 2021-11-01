5a0605369e1f945a2f2d43ae997ba7b3e03ac587
bduvidzon <79331969+bduvidzon@users.noreply.github.com>
2021-11-01 04:46:07 -0500

    Update ResearcherReservationSchedule.aspx.cs

--- ResearcherReservationSchedule.aspx.cs
+++ ResearcherReservationSchedule.aspx.cs
@@ -20,8 +20,8 @@ namespace Senior_Project
             { Label4.Text = Request.QueryString["Email"]; }
             con.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True";
             con.Open();
-            cmd.Connection = con;
-            cmd.CommandText = "SELECT * FROM [User] WHERE Email='" + Label4.Text.Trim() + "'";
+            SqlCommand cmd = new SqlCommand("SELECT * FROM[User] WHERE Email = @Email ", con);
+            cmd.Parameters.Add(new SqlParameter("Email", Label4.Text));
             dr = cmd.ExecuteReader();
             while (dr.Read())
             {
@@ -48,6 +48,12 @@ namespace Senior_Project
         {
             Response.Redirect("~/ResearchReservation.aspx", false);
             Response.Redirect("~/ResearchReservation.aspx?Email=" + Label4.Text);
+        }
+
+        protected void LinkButton4_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/ResearcherAccount.aspx", false);
+            Response.Redirect("~/ResearcherAccount.aspx?Email=" + Label4.Text);
         }
     }
 }
